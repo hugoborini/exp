d098ee684aff655c5b2e9ded15f4b5ac7a1f142a
hugo borini <hborini@localhost.localdomain>
2019-11-12 05:51:55 -0500

    change ex 2

--- tableau binomiale/ex_2.js
+++ tableau binomiale/ex_2.js
@@ -2,14 +2,13 @@ function array_x_y(t1, t2) {
   tab_x = [];
   tab_y = [];
   for (var j = 0; j < t2; j++) {
-    tab_y.push(0);
+    tab_y.push(Math.floor(Math.random() * 1000));
   }
   for (var i = 0; i < t1; i++) {
     tab_x.push(tab_y);
   }
-  console.log(tab_x[9][3]);
-
+  console.log(tab_x);
   return tab_x;
 }
 
-array_x_y(10, 4);
+array_x_y(10, 10);
